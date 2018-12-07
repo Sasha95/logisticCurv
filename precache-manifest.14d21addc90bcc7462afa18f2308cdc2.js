af1099ae75818703f99658ee644dc6f6ca97f88f
Мангир Александр <sasha982025@gmail.com>
2018-12-07 04:10:17 +0200

    Updates

---
+++ precache-manifest.14d21addc90bcc7462afa18f2308cdc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed2952e99a40ff76f532",
+    "url": "/Sasha95/logisticCurv/static/js/runtime~main.ed2952e9.js"
+  },
+  {
+    "revision": "47b11dd81a8fda058348",
+    "url": "/Sasha95/logisticCurv/static/js/main.47b11dd8.chunk.js"
+  },
+  {
+    "revision": "bea3f018f50b7d8b184b",
+    "url": "/Sasha95/logisticCurv/static/js/1.bea3f018.chunk.js"
+  },
+  {
+    "revision": "47b11dd81a8fda058348",
+    "url": "/Sasha95/logisticCurv/static/css/main.827b5a5e.chunk.css"
+  },
+  {
+    "revision": "86134d93dfd61413d1ac2baaf2b7b2f0",
+    "url": "/Sasha95/logisticCurv/index.html"
+  }
+];
